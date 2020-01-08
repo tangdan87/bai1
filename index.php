ae9cdc5864977132b7b97e9fac335580ad1d4856
Eric Petter <ericpetter@Erics-MacBook-Air.local>
2020-01-08 16:31:01 +0700

    cm1

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-
 <html>
-<head>
-	<title>hocWebBai1</title>
-</head>
 <body>
-	<p>chuong trinh test1</p>
-	<form action="index.php" method="get">
-		name: <input type="text" name="name"> <br>
-		<input type="submit">
-	</form>
+
+<?php 
+echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+?>
+
 </body>
 </html>
