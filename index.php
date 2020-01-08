8def4f45173c7e4d19f0097b5bebb1e3fbdc1923
Eric Petter <ericpetter@Erics-MacBook-Air.local>
2020-01-08 16:05:01 +0700

    commit test1

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-chuong trinh test
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hocWebBai1</title>
+</head>
+<body>
+	<p>chuong trinh test</p>
+	<form>
+		name: <input type="text" name="name"> <br>
+		<input type="submit">
+	</form>
+</body>
+</html>
