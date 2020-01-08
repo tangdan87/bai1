63c3a1168264078ae4b1ae9984cd74d8c60be409
Eric Petter <ericpetter@Erics-MacBook-Air.local>
2020-01-08 16:27:37 +0700

    cm1

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!-- <?php
 
-// echo "123";
-if (isset($_GET['name'])) {
-	$name = $_GET['name'];
-	echo "your name is {$name}";
-}
-
-?> -->
-
-<!DOCTYPE html>
 <html>
 <head>
 	<title>hocWebBai1</title>
